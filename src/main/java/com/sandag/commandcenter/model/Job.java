e3d35f31ed417260576a0775a0d416d576770010
Craig Sabbey <csabbey@gmail.com>
2013-12-11 17:29:47 -0500

    have hibernate working with a simple test

---
+++ src/main/java/com/sandag/commandcenter/model/Job.java
@@ -0,0 +1,69 @@
+package com.sandag.commandcenter.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="job")
+public class Job {
+
+	public enum Status {
+		QUEUED, RUNNING, COMPLETE, ARCHIVED, DELETED;
+
+		public String toString() {
+			switch(this) {
+				case QUEUED: return "Queued";
+				case RUNNING: return "Running";
+				case COMPLETE: return "Complete";
+				case ARCHIVED: return "Archived";
+				case DELETED: return "Deleted";
+			}
+			return "Unknown";
+		}		
+	}
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue
+	private Integer id;
+
+	@Column(name="model")
+	private String modelName;
+	
+	private Status status;
+	
+	@Column(name="status")
+	@Enumerated(EnumType.ORDINAL)
+    public Status getStatus() {
+		return status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	};
+
+	
+	
+	
+}
---
+++ src/test/java/com/sandag/commandcenter/model/JobTest.java
@@ -0,0 +1,46 @@
+package com.sandag.commandcenter.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.Test;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+  "classpath:/db.xml",
+})
+public class JobTest {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Test
+	public void configWorks() {
+		Session session = null;
+		try {
+			session = sessionFactory.openSession();
+			Job job = new Job();
+			assertNull(job.getId());
+			
+			session.save(job);
+			assertTrue(job.getId() > 0);
+	
+			Job retrievedJob = (Job) session.createCriteria(Job.class).setMaxResults(1).list().get(0);
+			assertEquals(job.getId(), retrievedJob.getId());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+	
+	
+	
+}
